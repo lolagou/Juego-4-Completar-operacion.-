7b96d450e2d033a3711dd98fdaae0eda02025669
Juanihb <juanipandarojo@gmail.com>
2024-06-26 09:05:25 -0300

    Instantiate trial

--- Juego 4/Assets/Scripts/ProductoAzar.cs
+++ Juego 4/Assets/Scripts/ProductoAzar.cs
@@ -7,17 +7,22 @@ public class ProductoAzar : MonoBehaviour
 {
     public Text[] optionsText;
     public GameObject[] objetos;
+    public float randomnumber;
+    public GameObject prueba; 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 position1 = new Vector3(-0.53f, 0.45f, -4.18f);
-        Vector3 position2 = new Vector3(2.23f, -0.61f, -3.07f);
+        Instantiate(prueba, new Vector3(2.23f, -0.61f, -3.07f), Quaternion.identity);
     }
-
+        
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    void RandomizePositions()
+    {
+         
+    }
 }
