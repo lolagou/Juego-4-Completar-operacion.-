86012d69583182c01c4a8c175da52f5ebc4e83f1
Juanihb <juanipandarojo@gmail.com>
2024-07-01 01:43:45 -0300

    TP Unity Arreglado (ahora si)

--- Juego 4/Assets/Scripts/ClickOnObjects.cs
+++ Juego 4/Assets/Scripts/ClickOnObjects.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class ClickOnObjects : MonoBehaviour
 {
-    public int valor;
-    public Text text;
-    int n = 0;
+    //public int valor;
+    //public Text text;
+    //int n = 0;
 
     void Start()
     {
-        valor = Random.Range(1, 21);
+        //valor = Random.Range(1, 21);
     }
 
 
@@ -22,18 +22,17 @@ public class ClickOnObjects : MonoBehaviour
 
     void OnMouseDown()
     {
-        if (n == 0)
-        {
-            text.fontStyle = FontStyle.Bold;
-            text.color = Color.blue;
-            n++;
-        }
-        else if (n == 1)
-        {
-            text.fontStyle = FontStyle.Normal;
-            text.color = Color.black;
-            n = 0;
-        }
+        //if (n == 0)
+        //{
+          //  text.fontStyle = FontStyle.Bold;
+          //  text.color = Color.blue;
+          //  n++;
+       // }
+       // else if (n == 1)
+       // {
+          //  text.fontStyle = FontStyle.Normal;
+          //  text.color = Color.black;
+          //  n = 0;
+       // }
     }
-
 }
--- Juego 4/Assets/Scripts/ProductoAzar.cs
+++ Juego 4/Assets/Scripts/ProductoAzar.cs
@@ -6,123 +6,59 @@ using UnityEngine.SceneManagement;
 
 public class ProductoAzar : MonoBehaviour
 {
-    public Button ResponderBtn;
+    //public Button ResponderBtn;
+    //public GameObject panelNotificaciones;
+    //public GameObject panelError;
+    //public GameObject panelRespuesta;
+    //public Text Btn_jugarOtraVez;
+    //public Text textNotificaciones;
+    //public Text Btn_MasJuegos;
+    //public int Selected = -1;
+    //public int Ecuacion;
 
+    public GameObject[] Objetos;
+    public Text valor1;
+    public Text valorTotal;
+    public Text Articulo1;
+    public Text Articulo2;
+    public Text Articulo3;
+    public Text[] valoresPosibles;
 
-    public GameObject panelNotificaciones;
-    public GameObject panelError;
-    public GameObject panelRespuesta;
-    public Text Btn_jugarOtraVez;
-    public Text textNotificaciones;
-    public Text Btn_MasJuegos;
-
-    public Text[] optionsText;
-    public GameObject[] objetos;
-    public GameObject prueba;
-    public float randomnumber;
-    public int[] arrayPrecios;
-
-
-    public Vector3 PosicionEquation;
-    public Vector3 PosicionObjeto1;
-    public Vector3 PosicionObjeto2;
-    public Vector3 PosicionObjeto3;
-    public Vector3 PosicionObjeto4;
-
-    //Vector3 coordenadasArriba = new Vector3(-279f, 125f, 0f);
-    //Vector3 coordenadasDerecha = new Vector3(-260f, -133f, 0f);
-    //Vector3 coordenadasMedio = new Vector3(12f, -133f, 0f);
-    //Vector3 coordenadasIzquierda = new Vector3(262f, -133f, 0f);
-
-    public int precioObjeto1;
-    public int precioObjeto2;
-    public int precioObjeto3;
-    public int precioObjeto4;
-    public int precioEquation;
-    public int Total; 
-
-    public Text txtPrecioObjeto1;
-    public Text txtPrecioObjeto2;
-    public Text txtPrecioObjeto3;
-    public Text txtPrecioObjeto4;
-    public Text EquationText;
-
-    public GameObject Objeto1;
-    public GameObject Objeto2;
-    public GameObject Objeto3;
-    public GameObject Objeto4;
-
-    public int Selected = -1;
-    public int Ecuacion;
-
-    private int Random_Number;
     // Start is called before the first frame update
     void Start()
     {
-        //Se crea una funcion RandomNumer donde se establece con un RandomRange la longuitud que pueden abarcar los elementos del array gameobject que es objetos
-        //Va desde el cero hasta la longuitud de los elementos por ello se utliza .lenght
-        //A cada objeto se lo iguala a la array con randomnumer
-        //Luego se utiliza un Instantiate el cual consta de tres partes (_enemyObject, new Vector2(0,8), Quaternion).  
-        //En el primero se pone el objeto, luego el vector que en este caso es la posicion y por ultimo la rotacion del objeto que se tuvo a transformar. 
-   
-        Random_Number = Random.Range(0, objetos.Length);
-        Objeto1 = objetos[Random_Number];
-        Instantiate(Objeto1, PosicionObjeto1, objetos[Random_Number].transform.rotation);
+        int dinero1 = Random.Range(0, 20);
+        int dinerototal = Random.Range(dinero1 + 1, dinero1 + 20);
+        int dinero2 = dinerototal - dinero1;
+        valor1.text = "$" + dinero1.ToString();
+        valorTotal.text = "$" + dinerototal.ToString();
+        int indiceCorrecto = Random.Range(0, 2);
+        for (int i = 0;  i < valoresPosibles.Length; i++)
+        {
+            if (i == indiceCorrecto)
+            {
+                valoresPosibles[indiceCorrecto].text = "$" + dinero2.ToString();
+            }
+            else
+            {
+                int random;
+                do
+                {
+                    random = Random.Range(0, 20);
+                } while (random == dinero2);
+                valoresPosibles[i].text = "$" + random.ToString();
+            }
+        }
 
-        Random_Number = Random.Range(0, objetos.Length);
-        Objeto2 = objetos[Random_Number];
-        Instantiate(Objeto2, PosicionObjeto2, objetos[Random_Number].transform.rotation);
+        int random1 = Random.Range(0, 4);
+        int random2 = Random.Range(0, 4);
+        int random3 = Random.Range(0, 4);
+        int random4 = Random.Range(0, 4);
 
-        Random_Number = Random.Range(0, objetos.Length);
-        Objeto3 = objetos[Random_Number];
-        Instantiate(Objeto3, PosicionObjeto3, objetos[Random_Number].transform.rotation);
-
-        Random_Number = Random.Range(0, objetos.Length);
-        Objeto4 = objetos[Random_Number];
-        Instantiate(Objeto4, PosicionObjeto4, objetos[Random_Number].transform.rotation);
-
-        //El precio del Objeto1 y se obtiene el component cuando se clickee en el objeto indicado y se le da un valor. 
-        
-        
-        precioObjeto1 = Objeto1.GetComponent<ClickOnObjects>().valor;
-        precioObjeto2 = Objeto2.GetComponent<ClickOnObjects>().valor;
-        precioObjeto3 = Objeto3.GetComponent<ClickOnObjects>().valor;
-        precioObjeto4 = Objeto4.GetComponent<ClickOnObjects>().valor;
-        
-
-        //El texto de cada objeto tiene ser igual al precio asignado a cada objeto. 
-
-        txtPrecioObjeto1.text = "$ " + precioObjeto1;
-        txtPrecioObjeto2.text = "$ " + precioObjeto2;
-        txtPrecioObjeto3.text = "$ " + precioObjeto3;
-        txtPrecioObjeto4.text = "$ " + precioObjeto4;
-
-
-        //Se crea un array de los precios, donde se indican todos los que hay en los corchetes. Luego se genera un RandomRange
-        //Y se hace el calculo que el precioTotal tiene que ser igual al equiation text mas alguno del array de los precios. 
-
-        //int[] arrayPrecios = { precioObjeto1, precioObjeto2, precioObjeto3, precioObjeto4 };
-        //int precioAzar = Random.Range(0, arrayPrecios.Length);
-        //int precioTotal = precioObjeto4 + arrayPrecios[precioAzar];
-
-        //txtPrecioObjeto1.text = ("$" + precioTotal);
-        //Total = precioTotal;
-
-        Ecuacion = objetos[Random.Range(1, objetos.Length)].GetComponent<ClickOnObjects>().valor;
-        EquationText.text = "$" + (precioObjeto1 + Ecuacion);
-
-    }
-
-    //void RandomNumer()
-    // {
-    //    int azar = Random.Range(0, objetos.Length);
-    //    Objeto1 = objetos[azar];
-    //    Objeto1.SetActive(true);
-    //}
-
-    public void Valor_del_Objeto(GameObject Objeto)
-    { 
-        
+        Instantiate(Objetos[random1], new Vector3(-5f, 5.13f, 0.68f), Objetos[random1].transform.rotation);
+        Instantiate(Objetos[random2], new Vector3(-3f, 0.47f, -3f), Objetos[random2].transform.rotation);
+        Instantiate(Objetos[random3], new Vector3(2.03f, 0.47f, -3f), Objetos[random3].transform.rotation);
+        Instantiate(Objetos[random4], new Vector3(6f, 0.47f, -3f), Objetos[random4].transform.rotation);
     }
         
     // Update is called once per frame
@@ -130,37 +66,33 @@ public class ProductoAzar : MonoBehaviour
     {
 
     }
-
     public void ButtonResponderClick()
     {
-        if (Selected == -1)
-        {
-            panelError.SetActive(true);
-            panelRespuesta.SetActive(false);
-        }
-        else if (Ecuacion == precioObjeto1 + objetos[Selected].GetComponent<ClickOnObjects>().valor)
-        {
-            panelNotificaciones.SetActive(true);
-            panelRespuesta.SetActive(false);
-            textNotificaciones.text = "El resultado es correcto";
-            Btn_jugarOtraVez.text = "REINICIAR DESAFIO";
-            Btn_MasJuegos.text = "Mas Juegos";
-        }
-        else
-        {
-            panelNotificaciones.SetActive(true);
-            panelRespuesta.SetActive(false);
-            textNotificaciones.text = "El resultado es incorrecto";
-            Btn_jugarOtraVez.text = "JUGAR OTRA VEZ";
-            Btn_MasJuegos.text = "Mas Juegos";
-        }
+       // if (Selected == -1)
+       // {
+       //     panelError.SetActive(true);
+       //     panelRespuesta.SetActive(false);
+       // }
+      //  else if (Ecuacion == precioObjeto1 + objetos[Selected].GetComponent<ClickOnObjects>().valor)
+      //  {
+       //     panelNotificaciones.SetActive(true);
+       //     panelRespuesta.SetActive(false);
+       //     textNotificaciones.text = "El resultado es correcto";
+       //     Btn_jugarOtraVez.text = "REINICIAR DESAFIO";
+      //      Btn_MasJuegos.text = "Mas Juegos";
+     //   }
+     //   else
+     //   {
+     //       panelNotificaciones.SetActive(true);
+      //      panelRespuesta.SetActive(false);
+       //     textNotificaciones.text = "El resultado es incorrecto";
+       //     Btn_jugarOtraVez.text = "JUGAR OTRA VEZ";
+       //     Btn_MasJuegos.text = "Mas Juegos";
+      //  }
     }
 
     public void Salir()
     {
-        SceneManager.LoadScene("Seleccionar Mas Juegos");
+       // SceneManager.LoadScene("Seleccionar Mas Juegos");
     }
-
-
-
 }
