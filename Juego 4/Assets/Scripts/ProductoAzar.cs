e72b8f152df3da0d1f49bf7a19ff135be2e7e262
Valentin-Alfie <48178972@est.ort.edu.ar>
2024-07-02 03:32:16 -0300

    TP Unity siguen faltando cosas

--- Juego 4/Assets/Scripts/ProductoAzar.cs
+++ Juego 4/Assets/Scripts/ProductoAzar.cs
@@ -6,61 +6,78 @@ using UnityEngine.UI;
 
 public class ProductoAzar : MonoBehaviour
 {
+    [System.Serializable]
+    public class ObjetoConValor
+    {
+        public GameObject objeto;
+        public int valor;
+    }
 
-    public GameObject[] Objetos;
+    public ObjetoConValor[] Objetos;
     public Text valor1;
     public Text valorTotal;
     public Text Articulo1;
     public Text Articulo2;
     public Text Articulo3;
     public Text[] valoresPosibles;
-
-    public Text[] optionsText;
-    public GameObject[] objetos;
-    public GameObject prueba;
-
-    public GameObject Objeto3;
-    public GameObject Objeto4;
-
-    public int Ecuacion;
-
-    private int Random_Number;
+    public int dinerototal;
+    public int indiceCorrecto;
 
     // Start is called before the first frame update
     void Start()
     {
         int dinero1 = Random.Range(0, 20);
-        int dinerototal = Random.Range(dinero1, dinero1 + 20);
-        int dinero2 = dinerototal - dinero1;
+        int Objeto1 = Random.Range(0, 4);
+        Objetos[Objeto1].valor = dinero1;
         valor1.text = "$" + dinero1.ToString();
+
+        dinerototal = Random.Range(dinero1, dinero1 + 20);
         valorTotal.text = "$" + dinerototal.ToString();
-        int indiceCorrecto = Random.Range(0, 2);
-        for (int i = 0;  i < valoresPosibles.Length; i++)
+
+        int dinero2 = dinerototal - dinero1;
+        int Objeto2;
+        if (dinero2 == dinero1)
         {
-            if (i == indiceCorrecto)
+            Objeto2 = Objeto1;
+        }
+        else
+        {
+            do
             {
-                valoresPosibles[indiceCorrecto].text = "$" + dinero2.ToString();
-            }
-            else
+                Objeto2 = Random.Range(0, 4);
+            } while (Objeto1 == Objeto2);
+        }
+        Objetos[Objeto2].valor = dinero2;
+
+        int Objeto3 = Random.Range(0, 4);
+        if (Objeto3 != Objeto2 && Objeto3 != Objeto1)
+        {
+            do
             {
-                int random;
-                do
-                {
-                    random = Random.Range(0, 20);
-                } while (random == dinero2);
-                valoresPosibles[i].text = "$" + random.ToString();
-            }
+                Objetos[Objeto3].valor = Random.Range(0, 20);
+            } while (Objetos[Objeto3].valor == Objetos[Objeto2].valor || Objetos[Objeto3].valor == Objetos[Objeto1].valor);
         }
 
-        int random1 = Random.Range(0, 4);
-        int random2 = Random.Range(0, 4);
-        int random3 = Random.Range(0, 4);
-        int random4 = Random.Range(0, 4);
+        int Objeto4 = Random.Range(0, 4);
+        if (Objeto4 != Objeto3 && Objeto4 != Objeto2 && Objeto4 != Objeto1)
+        {
+            do
+            {
+                Objetos[Objeto4].valor = Random.Range(0, 20);
+            } while (Objetos[Objeto4].valor == Objetos[Objeto3].valor || Objetos[Objeto4].valor == Objetos[Objeto2].valor || Objetos[Objeto4].valor == Objetos[Objeto1].valor);
+        }
+        indiceCorrecto = Random.Range(0, 3);
+        valoresPosibles[indiceCorrecto].text = "$" + Objetos[Objeto2].valor.ToString();
 
-        Instantiate(Objetos[random1], new Vector3(-5f, 5.13f, 0.68f), Objetos[random1].transform.rotation);
-        Instantiate(Objetos[random2], new Vector3(-3f, 0.47f, -3f), Objetos[random2].transform.rotation);
-        Instantiate(Objetos[random3], new Vector3(2.03f, 0.47f, -3f), Objetos[random3].transform.rotation);
-        Instantiate(Objetos[random4], new Vector3(6f, 0.47f, -3f), Objetos[random4].transform.rotation);
+        List<int> IndiceObjetos = new List<int> {0, 1, 2};
+        IndiceObjetos.Remove(indiceCorrecto);
+        valoresPosibles[IndiceObjetos[0]].text = "$" + Objetos[Objeto3].valor.ToString();
+        valoresPosibles[IndiceObjetos[1]].text = "$" + Objetos[Objeto4].valor.ToString();
+
+        Instantiate(Objetos[Objeto1].objeto, new Vector3(-5f, 5.13f, 0.68f), Objetos[Objeto1].objeto.transform.rotation);
+        Instantiate(Objetos[Objeto2].objeto, new Vector3(-3f, 0.47f, -3f), Objetos[Objeto2].objeto.transform.rotation);
+        Instantiate(Objetos[Objeto3].objeto, new Vector3(2.03f, 0.47f, -3f), Objetos[Objeto3].objeto.transform.rotation);
+        Instantiate(Objetos[Objeto4].objeto, new Vector3(6f, 0.47f, -3f), Objetos[Objeto4].objeto.transform.rotation);
     }
         
     // Update is called once per frame
